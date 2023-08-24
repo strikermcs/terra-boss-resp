fad939598382cb71844b1aa38bbd45e68c38e1d2
strikermcs <tellermcs@gmail.com>
2023-08-24 18:46:34 +0300

    fix bug with pusher

--- server/api/bosses/index.ts
+++ server/api/bosses/index.ts
@@ -4,19 +4,19 @@ export default defineEventHandler(async (event) => {
     if(event.method === 'POST') {
         const body = await readBody(event)
         
-        pusherServer.trigger('Bosses', 'boss:add', body)
+       await pusherServer.trigger('Bosses', 'boss:add', body)
     }
 
     if(event.method === 'PATCH') {
         const body = await readBody(event)
 
-        pusherServer.trigger('Bosses', 'boss:update', body)
+        await pusherServer.trigger('Bosses', 'boss:update', body)
     }
 
     if(event.method === 'PUT') {
         const body = await readBody(event)
         console.log(body)
-        pusherServer.trigger('Bosses', 'boss:delete', body)
+        await pusherServer.trigger('Bosses', 'boss:delete', body)
     }
    
 })
